825ab748dcb2e5101d2922059a64c868af3ce41a
AndreM84 <60691897+AndreM84@users.noreply.github.com>
2025-06-04 20:55:38 +0200

    Update building.py

--- src/dibs_computing_core/iso_simulator/model/building.py
+++ src/dibs_computing_core/iso_simulator/model/building.py
@@ -910,7 +910,7 @@ class Building(object):
             print(f"t_m_prev is of type {type(t_m_prev)}.")
 
         act_val1 = (
-                                 (Decimal(t_m_prev) * ((self.c_m / 3600.0) - 0.5 * (self.h_tr_3 + self.h_tr_em)))
+                                 (round(t_m_prev,16) * ((self.c_m / 3600.0) - 0.5 * (self.h_tr_3 + self.h_tr_em)))
                                  + self.phi_m_tot
                          )
 
