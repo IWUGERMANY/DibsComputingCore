471c7f0d5a3ae18667a3d028d12817f0cffcea13
AndreM84 <60691897+AndreM84@users.noreply.github.com>
2025-06-05 17:29:42 +0200

    Update building.py

--- src/dibs_computing_core/iso_simulator/model/building.py
+++ src/dibs_computing_core/iso_simulator/model/building.py
@@ -625,8 +625,8 @@ class Building(object):
             )  # Initialise Heating System Manager
 
             if self.has_heating_demand:
-                if not self.heating_supply_system:
-                    print(f"Not self.heating_supply_system is TRUE >> ID ist {self.scr_gebaeude_id}, self.heating_supply_system is: {self.heating_supply_system}.")
+                #if not self.heating_supply_system or not isinstance(self.heating_supply_system, (str, int, float)):
+                print(f"Not self.heating_supply_system is TRUE >> ID ist {self.scr_gebaeude_id}, self.heating_supply_system is: {self.heating_supply_system}.")
 
                 my_system = self.supply_mapping[self.heating_supply_system](
                     load=self.energy_demand,
